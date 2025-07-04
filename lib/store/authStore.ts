533f5f2aae84a3c3783194c418607fcd93c455c2
zadumka <90329284+zadumka@users.noreply.github.com>
2025-07-04 19:44:08 +0300

    Update authStore.ts

--- lib/store/authStore.ts
+++ lib/store/authStore.ts
@@ -5,10 +5,12 @@ type AuthStore = {
   isAuthenticated: boolean;
   user: User | null;
   setUser: (user: User) => void;
+  
 };
 
 export const useAuthStore = create<AuthStore>()((set) => ({
   isAuthenticated: false,
   user: null,
   setUser: (user: User) => set(() => ({ user, isAuthenticated: !!user })),
+  
 }));
