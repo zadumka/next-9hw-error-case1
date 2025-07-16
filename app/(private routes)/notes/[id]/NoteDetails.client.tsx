baa699fa2f8a9aa4450ac3a36dd5248c4aee5872
zadumka <90329284+zadumka@users.noreply.github.com>
2025-07-16 16:53:26 +0300

    Update NoteDetails.client.tsx

--- app/(private routes)/notes/[id]/NoteDetails.client.tsx
+++ app/(private routes)/notes/[id]/NoteDetails.client.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { useParams, useRouter } from 'next/navigation';
 import { useQuery } from '@tanstack/react-query';
-import { getNoteById } from '@/lib/api/clientApi';
+import { getNoteById } from '@/lib/api/serverApi';
 import css from './NoteDetails.module.css';
 
 const NoteDetailsClient = () => {
