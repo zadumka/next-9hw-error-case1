21a4318cf00a7b0fd03b9c7311a37225f03af113
zadumka <90329284+zadumka@users.noreply.github.com>
2025-10-14 15:04:40 +0300

    Update serverApi.ts

--- lib/api/serverApi.ts
+++ lib/api/serverApi.ts
@@ -1,42 +1,73 @@
-import { cookies } from 'next/headers';
-import { User } from '@/types/user';
-import { Note } from '@/types/note';
-import { nextServer } from './api';
+import { cookies } from "next/headers";
+import nextServer from "./api";
+import { User } from "@/types/user";
+import { Note } from "@/types/note";
+import { FetchNotesParams } from "./clientApi";
+import api from "./api";
 
-export const getMe = async (): Promise<User | null> => {
-  const { data } = await nextServer.get('/users/me');
-  return data;
-};
-
-export interface FetchNotesProps {
-  search?: string;
-  page?: number;
-  tag?: string;
-}
-
-export interface FetchNotesResponse {
+interface NotesHttpResponse {
   notes: Note[];
   totalPages: number;
 }
 
-export const fetchNotes = async ({ search, page, tag }: FetchNotesProps) => {
-  const response = await nextServer.get<FetchNotesResponse>('/notes', {
-    params: {
-      ...(search !== '' && { search }),
-      page,
-      perPage: 12,
-      ...(tag && { tag }),
-    },
+export const checkServerSession = async () => {
+  const cookieHeader = await getCookiesHeader();
+  const res = await api.get("/auth/session", {
+    headers: { Cookie: cookieHeader },
   });
-  return response.data;
+  return res;
 };
 
-export const checkSession = async () => {
-  const data = await nextServer.get('/auth/session');
+export async function getCookiesHeader(): Promise<string> {
+  const cookieStore = await cookies();
+  const cookieStr = cookieStore
+    .getAll()
+    .map(
+      ({ name, value }: { name: string; value: string }) => `${name}=${value}`
+    )
+    .join("; ");
+
+  return cookieStr;
+}
+
+export const getServerMe = async (): Promise<User> => {
+  const cookieHeader = await getCookiesHeader();
+  const { data } = await api.get("/users/me", {
+    headers: { Cookie: cookieHeader },
+  });
   return data;
 };
 
-export const fetchNoteById = async (id: string) => {
-  const { data } = await nextServer.get(`/notes/${id}`);
+export const fetchNotes = async ({
+  tag,
+  search,
+  page = 1,
+  perPage = 12,
+}: FetchNotesParams): Promise<NotesHttpResponse> => {
+  const cookieStore = cookies();
+  const res = await nextServer.get<NotesHttpResponse>("/notes", {
+    params: { tag, page, perPage, ...(search?.trim() ? { search } : {}) },
+    headers: {
+      Cookie: cookieStore.toString(),
+    },
+  });
+
+  return {
+    notes: res.data.notes,
+    totalPages: res.data.totalPages,
+  };
+};
+
+export const fetchNoteById = async (id: string): Promise<Note> => {
+  const cookieStore = await cookies();
+  const cookieHeader = cookieStore
+    .getAll()
+    .map(({ name, value }) => `${name}=${value}`)
+    .join("; ");
+
+  const { data } = await nextServer.get<Note>(`/notes/${id}`, {
+    headers: { Cookie: cookieHeader },
+  });
+
   return data;
 };
